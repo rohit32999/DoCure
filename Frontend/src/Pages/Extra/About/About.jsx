6e6a8d8b4e71cf0f08c03b0e455a55f69412ba81
Aratrik123 <aratrik.bandyopadhyay.21@aot.edu.in>
2024-02-04 09:39:05 +0530

    about section changed

--- Frontend/src/Pages/Extra/About/About.jsx
+++ Frontend/src/Pages/Extra/About/About.jsx
@@ -5,7 +5,9 @@ import vision from '../../../assets/images/vision.png';
 const About = () => {
     return (
         <div className='container'>
-            <h1 className='text-5xl my-5 text-center  font-bold'>About us</h1>
+            <br></br>
+            //<h1 className='text-5xl my-5 text-center font-bold'>About us</h1>
+            <section>
             <div className='grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1'>
                 <div className="card  bg-base-100 rounded-md ">
                     <figure><img src={mission} className='rounded-lg image-full' alt="Shoes" /></figure>
@@ -14,15 +16,16 @@ const About = () => {
                     <figure><img src={vision} className='rounded-lg' alt="Shoes" /></figure>
                 </div>
             </div>
+            </section>
 
-            <p className='my-6 px-10'>DoCure is an online Hospital Managemnt System that caters to the day to day operations of the hospitals . </p>
-
-            <p className='px-10'>DoCure offers on-demand GP and specialist consultations with verified and certified doctors, online prescriptions, medicine delivery, pathology/ diagnostics tests, all while building valuable Electronic Health Records</p>
-
-            <p className='my-6 px-10'>DocService is vertically integrated with pharmacies/ medicine delivery suppliers, labs/ diagnostics centres, and payment providers for an end-to-end digital health experience</p>
-
-            <p className='px-10'>DocService systematically focuses on expanding access to affordable healthcare services to all. We promise to improve our solutions continuously through our scalable technology and data analytics capabilities. Our passionate team is committed to excellence and work extremely hard to ensure we make a positive contribution to the healthcare of the future</p>
-
+            <p className='my-6 px-10'>DoCure is an online Hospital Management System that caters to the day to day operations of the hospitals.
+            DoCure offers specialist consultations with verified and certified doctors, online prescriptions, medicine delivery, 
+            pathology and diagnostics tests, all while building valuable Electronic Health Records. DoCure is vertically integrated with pharmacies/ 
+            medicine delivery suppliers, labs/ diagnostics centres, and payment providers for an end-to-end digital health experience. DoCure 
+            systematically focuses on expanding access to affordable healthcare services to all. We promise to improve our 
+            solutions continuously through our scalable technology and data analytics capabilities. Our passionate team is 
+            committed to excellence and work extremely hard to ensure we make a positive contribution to the healthcare of 
+            the future.</p>
         </div>
     );
 };
